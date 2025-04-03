48c0bfba77a53f14864d172fdde6ec16de1a99a8
klnwlks <kalinawlukasaombebis@gmail.com>
2025-04-03 22:11:46 +0800

    simplification

--- backend/ragserver/models.py
+++ backend/ragserver/models.py
@@ -2,9 +2,3 @@ from django.db import models
 
 # Create your models here.
 from django.contrib.postgres.fields import ArrayField
-
-class Conversation(models.Model):
-    title = models.CharField()
-    made = models.DateField()
-    messages = models.JSONField(default=list, blank=True)
-    f_documents = models.JSONField(default=list, blank=True)
--- backend/ragserver/views.py
+++ backend/ragserver/views.py
@@ -4,28 +4,10 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
 # Create your views here.
-@api_view(['GET', 'POST', 'PUT'])
-def conversation_list(request):
-
-    # should return all conversation titles
-    if request.method == 'GET':
-        # if no specific convo to get, return all objects
-        if request.param == 'wala':
-          conversations = Conversation.objects.all()
-          serializer = ConversationSerializer(conversations, many=True)
-          return Response(serializer.data)
-
-        cid = request.param.id 
-        # find object
-        # return object
-
-    # should create a new conversation
-    elif request.method == 'POST':
-        serializer = ConversationSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    # should update conversation with new messagea
-    elif request.method == 'PUT':
+@api_view(['POST'])
+def convo(request):
+  # v simple implementation
+  # i get a message, i run the message thru the database, it returns a message for me to display
+  if request.method == 'POST':
+    data = request.data
+  
